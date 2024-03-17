182e905167ed194d3d04e5877b9f335397a616fe
守望 <61259428+swxk521@users.noreply.github.com>
2024-03-18 01:37:30 +0800

    Delete luci-app-speedlimit directory

--- luci-app-speedlimit/luasrc/controller/speedlimit.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.speedlimit", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/speedlimit") then return end
-	entry({"admin", "control"}, firstchild(), "Control", 50).dependent = false
-	entry({"admin", "control", "speedlimit"}, cbi("speedlimit"), _("网速限制"), 20).dependent = true
- end
