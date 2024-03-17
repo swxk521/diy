e5ad8f074f8419cc6c1f13544ea902c197434538
守望 <61259428+swxk521@users.noreply.github.com>
2024-03-18 01:50:02 +0800

    Update timecontrol.lua

--- luci-app-timecontrol/luasrc/controller/timecontrol.lua
+++ luci-app-timecontrol/luasrc/controller/timecontrol.lua
@@ -2,10 +2,8 @@ module("luci.controller.timecontrol", package.seeall)
 
 function index()
     if not nixio.fs.access("/etc/config/timecontrol") then return end
-
-    entry({"admin", "control"}, firstchild(), "Control", 50).dependent = false
-    entry({"admin", "control", "timecontrol"}, cbi("timecontrol"), _("Internet Time Control"), 100).dependent = true
-    entry({"admin", "control", "timecontrol", "status"}, call("status")).leaf = true
+    entry({"admin", "services", "timecontrol"}, cbi("timecontrol"), _("Internet Time Control"), 120).dependent = true
+    entry({"admin", "services", "timecontrol", "status"}, call("status")).leaf = true
 end
 
 function status()
