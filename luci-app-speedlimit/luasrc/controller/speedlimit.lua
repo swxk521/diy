26651d23f129d2e3014d49ea9bc94ad9d8464b59
守望 <61259428+swxk521@users.noreply.github.com>
2024-04-07 21:35:05 +0800

    Update speedlimit.lua

--- luci-app-speedlimit/luasrc/controller/speedlimit.lua
+++ luci-app-speedlimit/luasrc/controller/speedlimit.lua
@@ -2,6 +2,5 @@ module("luci.controller.speedlimit", package.seeall)
 
 function index()
 	if not nixio.fs.access("/etc/config/speedlimit") then return end
-	entry({"admin", "control"}, firstchild(), "Control", 50).dependent = false
-	entry({"admin", "control", "speedlimit"}, cbi("speedlimit"), _("网速限制"), 20).dependent = true
+	entry({"admin", "services", "speedlimit"}, cbi("speedlimit"), _("网速限制"), 120).dependent = true
  end
